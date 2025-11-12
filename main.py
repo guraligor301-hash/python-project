8e82ada6dba102b305698bae8eee6c33a5c7eda1
Ігор <guraligor301@gmail.com>
2025-11-12 04:11:47 +0200

    Initial commit: added main.py and .gitignore

---
+++ main.py
@@ -0,0 +1,15 @@
+
+my_list = [1, 2, 2, 3, 4]
+
+no_duplicates = list(set(my_list))
+print("Без дублікатів:", no_duplicates)
+
+
+reversed_list = my_list[::-1]
+print("Розвернутий список:", reversed_list)
+
+
+a = 2
+b = 3
+sum_ab = a + b
+print("Сума 2 + 3:", sum_ab)
