fcab3e243b467ac462870a40fd5aa00ace7902db
Ігор <guraligor301@gmail.com>
2025-11-12 04:22:48 +0200

    Додано елемент 7 до списку

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 
 my_list = [1, 2, 2, 3, 4]
 
+my_list.append(7)
+print("Після додавання елемента 7:", my_list)
+
+if 3 in my_list:
+    my_list.remove(3)
+print("Після видалення елемента 3:", my_list)
+
+print("Довжина списку зараз:", len(my_list))
+
 no_duplicates = list(set(my_list))
 print("Без дублікатів:", no_duplicates)
 
